e7a4899e13a758bba13d826db0a36da6a56e6dbe
Radoslav Rajčan <radoslav.rajcan@gmail.com>
2013-01-06 14:08:50 +0100

    Upravene normy a pridana jacobiho metoda

--- scripts/p05_normy_matice.py
+++ scripts/p05_normy_matice.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
+
 import numpy as np
+
+def abs_sum(zoznam):
+    """
+    Vracia sucet absolutnych hodnot vsetkych prvkov v zozname.
+    """
+    return sum([abs(prvok) for prvok in zoznam])
 
 def riadkova_norma(matica):
     ''' Vrati vysledok z riadkovej normy matice (maximum zo suctov prvkov v
@@ -8,13 +15,21 @@ def riadkova_norma(matica):
     >>> m = np.array([[1,2,3],[0,1,2],[-10,2,-3]])
     >>> riadkova_norma(m)
     15
+    >>> b = [15, 16, 1]
+    >>> riadkova_norma(b)
+    32
     '''
-    temp = []
-    for riadok in matica:
-        temp.append(sum([abs(prvok) for prvok in riadok]))
-    return max(temp)
+    matica = np.array(matica)
+    if type(matica[0]) == list:
+        temp = []
+        for riadok in matica:
+            temp.append(abs_sum(riadok))
+        return max(temp)
+    else:
+        return abs_sum(matica)
 
 def stlpcova_norma(matica):
+    #TODO pre vektory funguje rovnako ako riadkova neviem ako to ma byt
     ''' Vrati vysledok zo stlpcovej normy matice (maximum zo suctov prvkov v
         jednotlivych stlpcoch matice
 
@@ -22,20 +37,23 @@ def stlpcova_norma(matica):
     >>> stlpcova_norma(m)
     11
     '''
-
-    matica = np.array(matica)
-    temp = []
-    for stlpec in matica.transpose():
-        temp.append(sum([abs(prvok) for prvok in stlpec]))
-    return max(temp)
+    matica = np.array(matica).transpose()
+    if type(matica[0]) == list:
+        temp = []
+        for stlpec in matica:
+            temp.append(sum([abs(prvok) for prvok in stlpec]))
+        return max(temp)
+    else:
+        return abs_sum(matica)
 
 def frobeniova_norma(matica):
+    #TODO nefunguje na vektory
     '''
     >>> m = np.array([[1,2,3],[0,1,2],[-10,2,-3]])
     >>> round(frobeniova_norma(m), 4)
     11.4891
     '''
-
+    matica = np.array(matica)
     temp = []
     for riadok in matica:
         temp.append(sum([pow(prvok, 2) for prvok in riadok]))
--- scripts/p06_jacobiho_metoda.py
+++ scripts/p06_jacobiho_metoda.py
@@ -1,1 +1,75 @@
-__author__ = 'rado'
+#!/usr/bin/python
+import argparse
+import numpy as np
+
+import p05_normy_matice as normy
+
+def jacoby(A, b, x, presnost=2, verbose=False):
+    k = 0
+    x_tmp = x[:]
+    presnost = 10**(-presnost)
+
+    if verbose:
+        print("A: {}".format(A))
+        print("b: {}".format(b))
+        print("x: {}".format(x))
+        print("#"*10 + " Zaciatok vypoctu " + "#"*10)
+
+    while True:
+        k += 1
+        for i in range(len(A[0])):  # pocet riadkov
+            sum_1 = sum_2 = 0
+            for j in range(i):
+                sum_1 += A[i][j] * x[j]
+            for j in range(i+1, len(A[0])):
+                sum_2 += A[i][j] * x[j]
+            x_tmp[i] = 1.0/A[i][i] * float(b[i] - sum_1 - sum_2)
+
+        if verbose:
+            print("Momentalna presnost: {}".format(
+                abs(normy.riadkova_norma(np.array(x_tmp)) -
+                    normy.riadkova_norma(np.array(x)))))
+            print("k{}\nx = {}".format(k, x))
+
+        if abs(normy.riadkova_norma(np.array(x_tmp)) -
+               normy.riadkova_norma(np.array(x))) <= presnost and k > 2:
+            print('-'*20)
+            print("Vysledok je: {}".format(x_tmp))
+            print("Pouzitych bolo {} iteracii a presnost " \
+                  "vysledku je +-{}".format(k, presnost))
+            break
+
+        x = x_tmp[:]
+
+'''
+A = [[11, 2,  1],
+     [1, 10,  2],
+     [2,  3, -8]]
+b = [15, 16, 1]
+x = [0, 0, 0]
+jacoby(A, b, x, 5, True)'''
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true", default=False,
+                        help="Zapina rozsireny vypis.")
+    parser.add_argument("A", help="Matica A napr. [[1,2],[3,4]]")
+    parser.add_argument("b", help="Vektor pravej strany [3, 4, 5]")
+    parser.add_argument("x", help="Pociatocna aproximacia [0, 0, 0]")
+    parser.add_argument("presnost", type=int, help="Na kolko desatinych "\
+                        "miest. (Musi byt vacsie ako 0)", default=2)
+
+    args = parser.parse_args()
+
+    try:
+        A = eval(args.A)
+        b = eval(args.b)
+        x = eval(args.x)
+    except SyntaxError as err:
+        print("Nespravne zadany vstup.")
+        parser.print_help()
+    else:
+        if (args.presnost > 0):
+            jacoby(A, b, x, int(args.presnost), args.verbose)
+        else:
+            parser.print_help()
